a6f55713e861a1432b72ca9c7c63362e33c2c0de
nithin nambiar <nithin@nithins-MBP.lan>
2024-04-13 13:42:15 +0100

    first commit

---
+++ src/main/java/com/example/langchain4j/Langchain4jExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.langchain4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Langchain4jExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Langchain4jExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/langchain4j/Langchain4jExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.langchain4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Langchain4jExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
